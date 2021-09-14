d4020929744caad7d6b695f126ad0d7a1a4b8d9a
IrynaDemianenko <90711498+IrynaDemianenko@users.noreply.github.com>
2021-09-14 23:48:08 +0300

    Add files via upload
    Вторая домашняя работа

---
+++ Homework2.py
@@ -0,0 +1,18 @@
+#task1
+print(8/2+5-(-3/2))
+
+#task2
+print('"Khal Drogo\'s favorite word is "athjahakar""')
+
+#task3
+print('Winter'+' '+'came'+' '+'for'+' '+'the'+' '+'House'+' '+'of'+' '+'Frey.')
+
+#task4
+word = 'First text'
+print(word)
+word = 'Second text'
+print(word)
+
+#task5
+name = input('What is your name: ')
+print('Hello!', name)
