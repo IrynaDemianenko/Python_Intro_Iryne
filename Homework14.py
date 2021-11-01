adcaba882150d37faedbbc642f0db15e9c73bd68
IrynaDemianenko <90711498+IrynaDemianenko@users.noreply.github.com>
2021-11-01 19:12:06 +0200

    Add files via upload

--- Homework14.py
+++ Homework14.py
@@ -1,12 +1,10 @@
 with open('in.txt', 'r') as file:
     file_contents = file.read()
 
-
-newlist = []
-file_contents.split()
-print(file_contents)
-newlist=list(filter(lambda x: len(x)>5, file_contents))
-newlist1 = ''.join(str(e) for e in newlist)
+file_new = file_contents.split()
+newlist = list(filter(lambda x: len(x)>=5, file_new))
+print(newlist)
+newlist1 = ' '.join(map(str, newlist))
 print(newlist1)
 with open('out.txt', 'w') as file:
     file.write(newlist1)
